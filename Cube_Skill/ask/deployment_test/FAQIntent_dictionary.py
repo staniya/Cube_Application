2a93425ff40c8d9d4070d3bf51e453c8116bd54a
Shinnosuke Taniya <staniya@Shinnosukes-MacBook-Air.local>
2017-12-19 14:10:18 +0530

    initial commit

---
+++ Cube_Skill/ask/deployment_test/FAQIntent_dictionary.py
@@ -0,0 +1,71 @@
+html_text = {
+
+    "Lazy Money": "Let's tackle the money that sits around in your bank account. "
+                  "You may be keeping it there because you have a big expense like a vacation or taxes "
+                  "or insurance coming up, or just because you don’t have the time. And while it sits there, "
+                  "it earns you a bit but not enough to beat inflation. And, why leave it there anyway? "
+                  "When we can make it earn a lot more without any work, without any lock in like FDs."
+                  "We will fix this together by moving your money to a liquid fund where your money is safe"
+                  " and you earn 6.5-7% - a lot more than your bank account. Plus, you can bring up to 50,000"
+                  " anytime into your bank within seconds with one tap to withdraw cash from your ATM.",
+
+    "Let's Get Rich": "Investing your money is hard. Where do you invest? What if you need it in an emergency? "
+                      "Will it grow? Is it safe? Stocks are well regulated and can be used for emergencies "
+                      "unlike say real estate. Indian stocks are a good place if you believe in the Indian "
+                      "economy. But how do you pick between all the options and over 2500 mutual funds? "
+                      "Or what if you want to invest abroad on the tech giants like Google, Apple, Amazon, "
+                      "Facebook, Microsoft? Or bet on the China story? Cube is there to make this easy. "
+                      "Pick your preferred option from our selected list of both Indian and international "
+                      "mutual funds. You can quickly see the risk level and the returns they have provided "
+                      "over the last 3 years. You have the chance to change the fund every time you add "
+                      "money to it, but understanding this right now and selecting some options will make it "
+                      "easier every week. And the money always stays in your name inside the Mutual Fund that "
+                      "you select. From your bank account to the fund. And comes back to your bank when you "
+                      "ask for it. Let’s make some money.",
+
+    "Do Good": "Ever wondered what you could do to help the less fortunate? And wondered how you could "
+               "help consistently and via a reliable organisation? And ensure you could track whether your "
+               "money had made a real impact? Us too. And this inspired us to build something we are "
+               "proud of - an automated micro lending program along with Milaap (a RBI registered entity "
+               "with a long track record of being ethical and reliable). It’s so simple that it leaves "
+               "you shocked - you set an amount you want to loan out once a week, and everything else "
+               "is automatic. And the money comes back to you in a few months to keep lending to others. "
+               "You can see the number of people your goodness has impacted anytime inside Cube on the "
+               "home screen. And you can log into Milaap to see the status of each of your loans and "
+               "repayments. Let’s take 2 seconds to do this - It feels good to do good.",
+
+    "Full Automation": "Congratulations on setting up your money life - you will see the fruits of your work from "
+                       "Sunday morning as we automate the last paise of your money to make your life better. "
+                       "We can do one more thing for you - even automate the money movement from your bank to "
+                       "you mutual fund investments, bill payments, salaries, etc. i.e. no payment gateway steps. "
+                       "You are always in control and we only move whatever you approve - but that’s it - you do a "
+                       "quick 5 second review and approval and go back to your Sunday brunch plans. Our partner, "
+                       "BillDesk, keeps your approval on file and processes the money movement once we have your "
+                       "weekly approval. Ready for this? Let’s automate it all now.",
+
+    "Rewards": "We are all tired of the complex reward programs out there. Terms. Conditions. Expiry dates. At Cube, "
+               "we have simplified our rewards to match what is in your best interest. Without any fine print. "
+               "Examples: You handle more of your money in a responsible way - you level up. You automate more of "
+               "your bills and salaries to free up your time - you level up. You do good for others - you level up. "
+               "And to recognise the pioneers in this new era of money management (i.e. you), we do a frequent "
+               "sweepstakes to thank our users.We like researching fun life enhancing things to spend our money on. "
+               "And these make it to our sweepstakes as rewards. Who says window shopping on the internet doesn’t "
+               "pay off?",
+
+    "Cube ATM": "We all need funds in an emergency once in a while. That’s when we head to the nearest ATM. "
+                "In the past, this meant keeping some money in the bank for emergencies, and earning sub-optimal "
+                "returns. No more. With Cube, you can park your money in our Better Bank feature. "
+                "This money is available in a few seconds for you back in your bank account. And until you need it, "
+                "it earns you much higher returns. Magical. You can withdraw a maximum of Rs 50,000 or 95% of the "
+                "funds in your Better Bank balance.",
+
+    "Sell Investments": "Ever tried deciding what investments to sell and what to keep when you need cash? "
+                        "Can get confusing. All kinds of numbers flying around. You don’t know what is doing really "
+                        "well vs what is a bad bet. We have simplified this to show you just the core data you "
+                        "need to make a decision at such times. We are not advocates of short term investments, "
+                        "and suggest you leave your money in a category for at least 2-3 years to see how it pans out. "
+                        "But if you need it anytime - we show you the simple math to help you. "
+                        "Do know that MF investments that are held for more than 1 year enjoy favourable tax treatment "
+                        "in india. You should consult with your CA for the specifics of your case though."
+
+}
